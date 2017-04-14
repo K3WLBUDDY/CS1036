c3ef7ead076b240aabbb3f264284501f41cc56e9
Sruthik P <psruthik@gmail.com>
2017-04-14 19:35:37 +0000

    Bug Fixes

--- Macro.c
+++ Macro.c
@@ -4,8 +4,8 @@
 
 struct macro
 {
-	char mlabel[20], mopCode[20], moperand[20], mname[3];
-	//int mlength;
+	char mlabel[10][20], mopCode[10][20], moperand[10][20], mname[3];
+	int mlength;
 };
 
 int main()
@@ -16,42 +16,37 @@ int main()
 	output = fopen("Outputs/MacroOutput.txt", "w");
 
 	char label[20], opCode[20], operand[20];
-
-	printf("\n Hello2");
-
-	int macroCount=0, i=0;
-
-	printf("\n redsf");
+	int macroCount=0, i=0,j=0,len=0;
 
 	struct macro m[100];
 	
 	strcpy(opCode, "NULL");
-	printf("%s", opCode);
 
 	while(strcmp(opCode, "STOP")!=0)
 	{
-		printf("\n WOWOWO %s", opCode);
 		fscanf(input, "%s\t%s\t%s\t\n", label, opCode, operand);
-		printf("\n Test3");
-		printf("\n %s\t%s\t%s\t\n", label, opCode, operand);
-
 		if(strcmp(opCode, "MACRO")==0)
 		{
-			//m[macroCount].mlength=0;
+			len=0;
 			strcpy(m[macroCount].mname, operand);
+
+			printf("\n%s", m[macroCount].mname);
 			while(strcmp(opCode, "MEND")!=0)
 			{
 				fscanf(input, "%s\t%s\t%s\t\n", label, opCode, operand);
 
+				
 				if(strcmp(opCode, "MEND")!=0)
 				{
-					strcpy(m[macroCount].mlabel, label);
-					strcpy(m[macroCount].mopCode, opCode);
-					strcpy(m[macroCount].moperand, operand);
+					strcpy(m[macroCount].mlabel[len], label);
+					strcpy(m[macroCount].mopCode[len], opCode);
+					strcpy(m[macroCount].moperand[len], operand);
+					len++;
 				}
+				
 
 			}
-
+			m[macroCount].mlength=len;
 			macroCount++;
 		}
 	}
@@ -69,9 +64,13 @@ int main()
 			for(i=0;i<macroCount;i++)
 			{
 				if(strcmp(operand, m[i].mname)==0)
-						fprintf(output, "%s\t%s\t%s\t\n", m[i].mlabel, m[i].mopCode, m[i].moperand);
-					else
-						printf("\n\n %d %s", i,m[i].mname);
+				{
+					for(j=0; j<m[i].mlength; j++)
+					{
+						printf("\n %d", m[i].mlength);
+						fprintf(output, "%s\t%s\t%s\t\n", m[i].mlabel[j], m[i].mopCode[j], m[i].moperand[j]);
+					}
+				}
 			}
 
 		}
@@ -82,6 +81,8 @@ int main()
 
 	fclose(input);
 	fclose(output);
+
+	printf("\n Total No. of Macros : %d", macroCount);
 
 	return 0;
 }
