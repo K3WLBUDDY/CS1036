c5e964d4591792ce8c59d6dfff234657c8433fb2
Sruthik P <psruthik@gmail.com>
2017-04-14 19:40:53 +0000

    Final Macro Commit

--- Macro.c
+++ Macro.c
@@ -4,8 +4,8 @@
 
 struct macro
 {
-	char mlabel[10][20], mopCode[10][20], moperand[10][20], mname[3];
-	int mlength;
+	char mLabel[10][20], mOpCode[10][20], mOperand[10][20], mName[3];
+	int mLength;
 };
 
 int main()
@@ -16,6 +16,7 @@ int main()
 	output = fopen("Outputs/MacroOutput.txt", "w");
 
 	char label[20], opCode[20], operand[20];
+
 	int macroCount=0, i=0,j=0,len=0;
 
 	struct macro m[100];
@@ -25,28 +26,31 @@ int main()
 	while(strcmp(opCode, "STOP")!=0)
 	{
 		fscanf(input, "%s\t%s\t%s\t\n", label, opCode, operand);
+
 		if(strcmp(opCode, "MACRO")==0)
 		{
 			len=0;
-			strcpy(m[macroCount].mname, operand);
 
-			printf("\n%s", m[macroCount].mname);
+			strcpy(m[macroCount].mName, operand);
+
 			while(strcmp(opCode, "MEND")!=0)
 			{
-				fscanf(input, "%s\t%s\t%s\t\n", label, opCode, operand);
 
+				fscanf(input, "%s\t%s\t%s\t\n", label, opCode, operand);
 				
 				if(strcmp(opCode, "MEND")!=0)
 				{
-					strcpy(m[macroCount].mlabel[len], label);
-					strcpy(m[macroCount].mopCode[len], opCode);
-					strcpy(m[macroCount].moperand[len], operand);
+					strcpy(m[macroCount].mLabel[len], label);
+					strcpy(m[macroCount].mOpCode[len], opCode);
+					strcpy(m[macroCount].mOperand[len], operand);
+
 					len++;
 				}
 				
 
 			}
-			m[macroCount].mlength=len;
+
+			m[macroCount].mLength=len;
 			macroCount++;
 		}
 	}
@@ -56,6 +60,7 @@ int main()
 
 	while(strcmp(opCode, "STOP")!=0)
 	{
+
 		fscanf(input, "%s\t%s\t%s\t\n", label, opCode, operand);
 
 		if(strcmp(opCode, "CALL")==0)
@@ -63,13 +68,11 @@ int main()
 
 			for(i=0;i<macroCount;i++)
 			{
-				if(strcmp(operand, m[i].mname)==0)
+
+				if(strcmp(operand, m[i].mName)==0)
 				{
-					for(j=0; j<m[i].mlength; j++)
-					{
-						printf("\n %d", m[i].mlength);
-						fprintf(output, "%s\t%s\t%s\t\n", m[i].mlabel[j], m[i].mopCode[j], m[i].moperand[j]);
-					}
+					for(j=0; j<m[i].mLength; j++)
+						fprintf(output, "%s\t%s\t%s\t\n", m[i].mLabel[j], m[i].mOpCode[j], m[i].mOperand[j]);
 				}
 			}
 
