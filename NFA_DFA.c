0dcdd75fad4518001b600d8beda0bab2ef8e1dbb
Sruthik P <psruthik@gmail.com>
2017-04-17 01:43:50 +0000

    I/P Added for NFA_DFA

--- NFA_DFA.c
+++ NFA_DFA.c
@@ -8,11 +8,11 @@ int main()
 
 	char transition[10][10][10];
 
-	int transitionLength[10];
+	//int transitionLength[10][10];
 
 	int inputSymbol = 65;
 
-	printf("\n Enter the No. of Statess : ");
+	printf("\n Enter the No. of States : ");
 	scanf("%d", &states);
 
 	printf("\n Enter the No. of Inputs : ");
@@ -20,30 +20,36 @@ int main()
 
 	for(i=0; i<states; i++)
 	{
-		transitionLength[i][0]=0;
 
-		printf("\n Enter NFA for State %d", i);
-		printf("\n Seperate State Transitions with a Comma");
+		printf("\n Enter NFA for State %d : \n", i);
+		printf("\n Seperate State Transitions with a Comma\n");
 
 		for(j=0;j<inputs;j++)
 		{
-			printf("\n      %c", inputSymbol++);
-			printf("\n &d ----->  ", states);
 
-			scanf("%s", transition[i][j][k++]);
+			printf("\n      %c", inputSymbol++);
+			printf("\n %d ----->  ", i);
+			scanf("%s", &transition[i][j]);
+			printf("\n");
 		}
+
+		inputSymbol=65;
+	}
 
 
 	for(i=0; i<states; i++)
 	{
-		printf("\n Entered NFA For State %d : \n", i);
+		printf("\n\n Entered NFA For State %d : \n", i);
 
 		printf("\t");
 
 		for(j=0;j<inputs;j++)
 		{
-			printf("")
+			printf("\n      %c", inputSymbol++);
+			printf("\n %d ----->  %s", i, transition[i][j]);
 		}
+
+		inputSymbol=65;
 	}
 
 	return 0;
