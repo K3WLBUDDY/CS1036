1f4e5292c6749291c793446f6666e418912d6fba
Sruthik P <psruthik@gmail.com>
2017-04-21 17:55:17 +0000

    Added perfect First Buggy Follow

--- FirstFollow.c
+++ FirstFollow.c
@@ -2,15 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 
+char terminals[10][2], productions[10][10];
+//char nonTerminal[10];
+
+int n,i=0,x,j=0,k=0,len=0,y=0,success=0,z,pos,a,b;
+
+char s;
+
+
+void first(int index)
+{
+	cd 
+		int x = productions[index][0];
+
+		if(x>=97)
+			printf("%c", productions[index][0]);
+		else
+		{
+			s=productions[index][0];
+			x=productions[index][0];
+
+			l1:
+				for(k=0;k<n;k++)
+				{
+					z = terminals[k][0];
+					
+					if(x==z)
+					{
+						y=productions[k][0];
+						s=productions[k][0];
+					}
+					
+				}
+
+				if(y>=97)
+					printf("%c", s);
+				
+				else
+				{
+					x=s;
+					goto l1;
+				}
+		}
+}
+
 int main()
 {
-	int n,i=0,x,j=0,k=0;
-
-	char terminals[10][10], productions[10][10];
-	char nonTerminal[10];
-
-	char s;
-
+	
 	printf("\n Enter the No. of Productions in the Grammar : ");
 
 	scanf("%d", &n);
@@ -19,8 +57,7 @@ int main()
 	for(i=0;i<n;i++)
 		scanf("%s",terminals[i]);
 
-	printf("\n Enter the Productions for the Terminals : ");
-	printf("\n Seperate the Productions with a comma!\n");
+	printf("\n Enter the Productions for the Non Terminals : ");
 
 	for(i=0;i<n;i++)
 	{
@@ -38,46 +75,49 @@ int main()
 	for(i=0;i<n;i++)
 	{
 		printf("\n FIRST OF %s : ", terminals[i]);
-
-		int x = productions[i][0];
-		int y;
-
-		if(x>=97)
-			printf("%c", productions[i][0]);
-		else
-		{
-			j=i;
-			do
-			{
-				//printf("\n NOT FIRST");
-				//strcpy(nonTerminal, productions[i][0]);
-				s=productions[j][0];
-				//strcpy(nonTerminal, productions[i]);
-				//printf("%c", productions[i][0]);
-
-
-				for(k=0;k<n;k++)
-				{
-					if(strcmp(nonTerminal, terminals[j])==0)
-						break;
-				}
-				j=k;
-
-				y = productions[j][0];
-
-				if(y>=97)
-				{
-					printf("%c", productions[j][0]);
-					break;
-				}
-				else
-					continue;
-
-				//printf("%c", productions[j][0]);
-				//x=productions[j][0];
-			}
-			while(y<=97);
-		}
+		first(i);
 	}
+	
+	for(i=0;i<n;i++)
+	{
+		printf("\n FOLLOW OF %s : ", terminals[i]);
+
+		len=strlen(productions[i]);
+
+		for(j=0;j<len;j++)
+		{
+			x=productions[i][j];
+			y=terminals[i][0];
+			//if(strcmp(productions[i][j], terminals[i])==0)
+			if(x==y)
+			{
+				pos=j+1;
+
+				z=productions[i][pos];
+
+				if(z>=97)
+					printf("%c", productions[i][pos]);
+				else if(z>=65&&z<=90)
+					{
+						for(k=0;k<n;k++)
+						{
+							a=terminals[k][0];
+							b=productions[i][pos];
+
+							//if(strcmp(terminals[k], productions[i][pos])==0)
+							if(a==b)
+								first(k); 
+						}
+					}
+				success=1;
+			}
+		}
+		if(success==0)
+			printf("NULL");
+		success=0;
+
+	}
+	
+	return 0;
 
 }
