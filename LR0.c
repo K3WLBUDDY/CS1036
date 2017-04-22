d51839d07d17874039d1efcb34a7fe3a1e48b0a3
Sruthik P <psruthik@gmail.com>
2017-04-22 02:07:30 +0000

    Commented LR

--- LR0.c
+++ LR0.c
@@ -4,7 +4,7 @@
 char nonTerminals[10][2], productions[10][10];
 int n,i;
 int a,b;
-int setNo=1;
+int setNo=1; //Holds the Int Value for Item Set (I0, I1, I2...)
 int count;
 int j,x;
 char s;
@@ -16,14 +16,14 @@ void parse(int i, int pointer)
 
 	x=productions[i][pointer];
 	s=productions[i][pointer];
-	if(productions[i][pointer]=='\0')
+	if(productions[i][pointer]=='\0')//Check for Null Terminator after Shifting
 	{
-		printf("\n\n I%d ",setNo++);
+		printf("\n\n I%d ",setNo++);//setNo++ increments the Item Set Number
 		printf("\n %s -----> %s", nonTerminals[i], productions[i]);
 		printf(".");
 	}
 
-	else if(x<97)
+	else if(x<97)//Check for Non Terminal after Shifting
 	{
 		printf("\n I%d ",setNo++);
 		printf("\n %s -----> %c . %c", nonTerminals[i], productions[i][0], productions[i][1]);
@@ -34,15 +34,15 @@ void parse(int i, int pointer)
 		for(j=0;j<=n;j++)
 		{
 			a=nonTerminals[j][0];
-			if(a==x)
+			if(a==x)//Check for equality of Non Terminals
 			{	
 				printf("\n %s -----> .%s", nonTerminals[j], productions[j]);
 				break;
 				
 			}
 		}
-		parse(i,pointer+1);
-		parse(j, 1);
+		parse(i,pointer+1); //Parse for the Base Production
+		parse(j, 1);	//Parse for the New Production generated from Shifting "."
 	}
 }
 
@@ -58,12 +58,14 @@ int main()
 	for(i=0;i<n;i++)
 		scanf("%s", nonTerminals[i+1]);
 
+	//Get the Values at i+1 coz Augmented Grammar will be stored at index 0
+
 	printf("\n Enter the Productions : ");
 
 	for(i=0;i<n;i++)
 	{
 		printf("\n %s -----> ", nonTerminals[i+1]);
-		scanf("%s", productions[i+1]);
+		scanf("%s", productions[i+1]);//Will work only for a max of 2 productions per Non Terminal
 	}
 
 	printf("\n Entered Grammar : ");
@@ -71,22 +73,21 @@ int main()
 	for(i=0;i<n;i++)
 		printf("\n %s -----> %s ",nonTerminals[i+1], productions[i+1]);
 
-	strcpy(nonTerminals[0], "X");
+	strcpy(nonTerminals[0], "X");	//Copying Augmented Grammar to Index 0
 
-	strcpy(productions[0], nonTerminals[1]);
+	strcpy(productions[0], nonTerminals[1]);//Same as above
 
 	printf("\n\n LR (0) PARSER ITEM SETS : \n\n");
 
-	printf("\n I0 \n");
+	printf("\n I0 \n");	//Printing I0 Set Directly
 
 	for(i=0;i<n+1;i++)
-		printf("\n %s -----> .%s", nonTerminals[i], productions[i]);
+		printf("\n %s -----> .%s", nonTerminals[i], productions[i]); //Don't forget the "."
 
-	for(i=0;i<=n;i++)
+	for(i=0;i<=n;i++)	//Recursively Parsing for all the Productions
 	{
 		
-		pointer = strlen(productions[i]);
-		count=1;
+		count=1;	//Counter Variable to imitate the shifting of the "."
 		parse(i,count);
 	}
 
