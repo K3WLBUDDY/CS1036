0ea15b059764cf86464ae2ace673fde89f3981f7
Sruthik P <psruthik@gmail.com>
2017-05-07 17:48:39 +0000

    Added Final DAG with Nodes

--- DAG.c
+++ DAG.c
@@ -5,13 +5,17 @@
  *Interior Nodes must be either Operators or Results of Concatenated Operations
  */
 
-
-/* Currently the Code does not take into Consideration any Nodes/Childs/Pointers 
- * because the Record output is wrong and I'm lazy. Will add Nodes in some time
- */
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+
+#define UNDEFINED 69 //Ayy lmao
+
+struct node
+{
+	int leftChild, rightChild;
+	char contents[2];
+};
 
 int main()
 {
@@ -19,6 +23,7 @@ int main()
 	int length, leftPar=0, rightPar=0,count=0,len=0,found=0,i,j,k,childLength;
 	char childExp[100][10];
 	char finalArray[100];
+	struct node n[100];
 
 	printf("\n Enter an Expression with Parenthesis : "); //For brevity consider (a+b)*(c+d)
 	scanf("%s", expression);
@@ -44,7 +49,7 @@ int main()
 		else if(expression[i] == ')')
 		{
 			if(expression[i+2]=='('&& !isalnum(expression[i+1]))
-				finalArray[0]=expression[i+1];
+				n[0].contents[0]=expression[i+1];
 			rightPar++;
 		}
 	}
@@ -57,6 +62,9 @@ int main()
 
 	len=length-leftPar*2;//The Length of the Expression without the Parenthesis
 
+	for(i=0;i<len;i++)
+		n[i].leftChild=n[i].rightChild=UNDEFINED;
+
 	k=1;
 
 	for(i=0;i<count;i++)
@@ -65,18 +73,46 @@ int main()
 		{
 			if(!isalnum(childExp[i][j]))
 			{
-				finalArray[k]=childExp[i][j]; 
-				finalArray[++k]=childExp[i][j-1];//Gets the Operands of the Current Operator
-				finalArray[++k]=childExp[i][j+1];
+				n[k].contents[0]=childExp[i][j]; 
+				n[++k].contents[0]=childExp[i][j-1];//Gets the Operands of the Current Operator
+				n[++k].contents[0]=childExp[i][j+1];
 				k++;
 				break;
 			}
 		}
 	}
 
-	printf("\n Contents of DAG : ");
+	for(i=1;i<len;i++)
+	{
+		if(!isalnum(n[i].contents[0])&&n[0].leftChild==UNDEFINED)
+		{
+			n[0].leftChild=i;
+			n[i].leftChild=i+1;
+			n[i].rightChild=i+2;
+		}
+		else if(!isalnum(n[i].contents[0])&&n[0].rightChild==UNDEFINED)
+		{	
+			n[0].rightChild=i;
+			n[i].leftChild=i+1;
+			n[i].rightChild=i+2;
+		}
+	}
+
+
+
+	printf("\n Left Child %d \t Right Child %d \n", n[0].leftChild, n[0].rightChild);
+
+	printf("\n Contents of Direced Acyclic Graph : ");
+	printf("\n Node \t Value \t Left Child \t Right Child");
+
 	for(i=0;i<len;i++)
-		printf("\n\n  %c", finalArray[i]);
+	{
+		if(isalnum(n[i].contents[0]))
+			printf("\n\n   %d \t   %c \t    LEAF \t    LEAF", i, n[i].contents[0]);
+		else
+			printf("\n\n   %d \t   %c \t    %d \t              %d", i, n[i].contents[0], n[i].leftChild, n[i].rightChild);
+	}
+
 
 	return 0;
 }
